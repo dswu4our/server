7fdafed22d3ad5fd5e34e0abffcece9739a0e4ce
jiwon <ji_ji1997@naver.com>
2021-05-25 22:23:39 +0900

    'main' finish and making cook

--- models/ingredients.js
+++ models/ingredients.js
@@ -1,18 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Define Schemes
-const ingredientSchema = new mongoose.Schema({
-  ing_id: { type: Number, required: true, unique: true },
-  ing_name: { type: String, required: true },
-  ing_img: { type: String },
-  ing_expir: { type: Date }
-},
-{
-  timestamps: true
-});
+const ingredientSchema = new mongoose.Schema(
+  {
+    ing_id: { type: Number, required: true, unique: true },
+    ing_name: { type: String, required: true },
+    ing_img: { type: String },
+    ing_expir: { type: Date },
+    frozen: { type: Number },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 // Create Model & Export
-module.exports = mongoose.model('Ingredient', ingredientSchema);
+module.exports = mongoose.model("Ingredient", ingredientSchema);
 
 // // Create new todo document
 // ingredientSchema.statics.create = function (payload) {
@@ -21,27 +24,26 @@ module.exports = mongoose.model('Ingredient', ingredientSchema);
 //     // return Promise
 //     return todo.save();
 //   };
-  
+
 //   // Find All
 //   ingredientSchema.statics.findAll = function () {
 //     // return promise
 //     // V4부터 exec() 필요없음
 //     return this.find({});
 //   };
-  
+
 //   // Find One by todoid
 //   ingredientSchema.statics.findOneByTodoid = function (todoid) {
 //     return this.findOne({ todoid });
 //   };
-  
+
 //   // Update by todoid
 //   ingredientSchema.statics.updateByTodoid = function (todoid, payload) {
 //     // { new: true }: return the modified document rather than the original. defaults to false
 //     return this.findOneAndUpdate({ todoid }, payload, { new: true });
 //   };
-  
+
 //   // Delete by todoid
 //   ingredientSchema.statics.deleteByTodoid = function (todoid) {
 //     return this.remove({ todoid });
 //   };
-  
---
+++ routes/cook.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const Ingre = require("../models/ingredients");
+const recipe = require("../models/recipes");
+
+// 요리하기
+router.get("/", (req, res) => {
+  // 요리할 식재료 체크한거 배열로 가져와서
+  const array = req.body;
+  console.log("ing_id: " + array);
+  // 만들수 있는 레시피 검색
+  recipe.find(function (err, results) {
+    if (err) {
+      return res.status(500).send({ error: err.message });
+    }
+    res.status(200).json(results);
+  });
+});
+
+// 유투브 영상 리스트
+
+// 유투브 영상으로 넘겨주기
+
+module.exports = router;
