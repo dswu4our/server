724859ccc1b1e02e0488343c725a4794a2ccdca8
Ji Won <ji_ji1997@naver.com>
2021-09-03 15:13:44 +0900

    fix login

--- routes/login.js
+++ routes/login.js
@@ -8,13 +8,15 @@ router.post("/", (req, res) => {
 
   // if (User email이 있으면 user_id 찾기)
   Users.findOne({
-    email: body.email
+    email: body.email,
+    user_id: body.user_id
   }, function (err, result) {
+    // else User email 없으면 user_id 부여 & user_id 주기
     if (result === null)
     {
       Users.create({
         email: body.email,
-        user_id: body.id,
+        user_id: body.user_id,
         name: body.name
       });
       res.status(200).json("success");
@@ -24,17 +26,7 @@ router.post("/", (req, res) => {
     }
   })
   .select("user_id");
-  // else User email 없으면 user_id 부여 & user_id 주기
-
-  // Users.create({
-  //   email: body.email,
-  //   user_id: body.id,
-  //   name: body.name
-  // }, function (err, results) {
-  //   res.status(200).json("create ok");
-  // });
-
-  // res.send("access code 지급 완료");
+  
 });
 
 module.exports = router;
